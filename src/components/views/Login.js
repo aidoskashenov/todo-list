6480a3c7743a168cf3f2fdbd23b8e93ad2962fd0
Manav Misra <manavm@visionwebsoft.com>
2020-08-27 12:19:07 -0500

    :recycle: Use Formik component instead of multiple fxns.

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -1,51 +1,50 @@
 import React from "react"
 
-import { useFormik } from "formik"
+import { Formik } from "formik"
 
 import * as Yup from "yup"
 
-export const Login = () => {
-  const formik = useFormik({
-    // This is like 'setState'
-    initialValues: {
+export const Login = () => (
+  <Formik
+    initialValues={{
       email: "",
       name: "",
       pass: "",
-    },
-    validationSchema: Yup.object({
+    }}
+    validationSchema={Yup.object({
       name: Yup.string().required("Name is required!"),
       email: Yup.string()
         .email("Invalid email address!")
         .required("Email is required!"),
       pass: Yup.string().min(6).required("Pass is required!"),
-    }),
-    onSubmit: (values) => {
-      // This is like utility fxn. that gathers all values
+    })}
+    onSubmit={(values, { setSubmitting }) => {
       console.log("submission", values)
-    },
-  })
+      setSubmitting(false)
+    }}
+  >
+    {(formik) => (
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" {...formik.getFieldProps("name")} />
+        {formik.touched.name && formik.errors.name ? (
+          <p>{formik.errors.name}</p>
+        ) : null}
 
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      <label htmlFor="name">Name</label>
-      <input type="text" id="name" {...formik.getFieldProps("name")} />
-      {formik.touched.name && formik.errors.name ? (
-        <p>{formik.errors.name}</p>
-      ) : null}
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" {...formik.getFieldProps("email")} />
+        {formik.touched.email && formik.errors.email ? (
+          <p>{formik.errors.email}</p>
+        ) : null}
 
-      <label htmlFor="email">Email</label>
-      <input type="email" id="email" {...formik.getFieldProps("email")} />
-      {formik.touched.email && formik.errors.email ? (
-        <p>{formik.errors.email}</p>
-      ) : null}
+        <label htmlFor="pass">Password</label>
+        <input type="password" id="pass" {...formik.getFieldProps("pass")} />
+        {formik.touched.pass && formik.errors.pass ? (
+          <p>{formik.errors.pass}</p>
+        ) : null}
 
-      <label htmlFor="pass">Password</label>
-      <input type="password" id="pass" {...formik.getFieldProps("pass")} />
-      {formik.touched.pass && formik.errors.pass ? (
-        <p>{formik.errors.pass}</p>
-      ) : null}
-
-      <button type="submit">Submit</button>
-    </form>
-  )
-}
+        <button type="submit">Submit</button>
+      </form>
+    )}
+  </Formik>
+)
