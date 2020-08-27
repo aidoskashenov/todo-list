9e76dc24312814de203e21f9b50fddf4852b62a3
Manav Misra <manavm@visionwebsoft.com>
2020-08-27 12:50:36 -0500

    :lipstick: Use Bulma 'section' class

--- src/components/views/Login.js
+++ src/components/views/Login.js
@@ -5,54 +5,59 @@ import { Formik, Field, Form, ErrorMessage } from "formik"
 import * as Yup from "yup"
 
 export const Login = () => (
-  <Formik
-    initialValues={{
-      email: "",
-      name: "",
-      pass: "",
-    }}
-    validationSchema={Yup.object({
-      name: Yup.string().required("Name is required!"),
-      email: Yup.string()
-        .email("Invalid email address!")
-        .required("Email is required!"),
-      pass: Yup.string().min(6).required("Pass is required!"),
-    })}
-    onSubmit={(values, { setSubmitting }) => {
-      console.log("submission", values)
-      setSubmitting(false)
-    }}
-  >
-    <Form>
-      <div className="field">
-        <label htmlFor="name">Name</label>
-        <div className="control">
-          <Field name="name" type="text" />
-          <p className="help is-danger">
-            <ErrorMessage name="name" />
-          </p>
+  // TODO: Add state and a toggle button to switch between 'new account' vs 'login'
+  <section className="section">
+    <h2 className="title">Login/Create Account</h2>
+    <Formik
+      initialValues={{
+        email: "",
+        name: "",
+        pass: "",
+      }}
+      validationSchema={Yup.object({
+        name: Yup.string().required("Name is required!"),
+        email: Yup.string()
+          .email("Invalid email address!")
+          .required("Email is required!"),
+        pass: Yup.string().min(6).required("Pass is required!"),
+      })}
+      onSubmit={(values, { setSubmitting }) => {
+        console.log("submission", values)
+        setSubmitting(false)
+      }}
+    >
+      <Form>
+        <div className="field">
+          <label htmlFor="name">Name</label>
+          <div className="control">
+            <Field name="name" type="text" />
+            <p className="help is-danger">
+              <ErrorMessage name="name" />
+            </p>
+          </div>
         </div>
-      </div>
 
-      <div className="field">
-        <label htmlFor="email">Email</label>
-        <div className="control">
-          <Field name="email" type="email" />
-          <ErrorMessage name="email" />
+        <div className="field">
+          <label htmlFor="email">Email</label>
+          <div className="control">
+            <Field name="email" type="email" />
+            <ErrorMessage name="email" />
+          </div>
         </div>
-      </div>
 
-      <div className="field">
-        <label htmlFor="name">Password</label>
-        <div className="control">
-          <Field name="pass" type="password" />
-          <ErrorMessage name="pass" />
+        <div className="field">
+          <label htmlFor="name">Password</label>
+          <div className="control">
+            <Field name="pass" type="password" />
+            <ErrorMessage name="pass" />
+          </div>
         </div>
-      </div>
 
-      <button type="submit" className="button is-success">
-        Submit
-      </button>
-    </Form>
-  </Formik>
+        <button type="submit" className="button is-success">
+          Submit
+        </button>
+      </Form>
+    </Formik>
+    {/* TODO: Add 'toggle button' to switch state and show 'name input' or not */}
+  </section>
 )
