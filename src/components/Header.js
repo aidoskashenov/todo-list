6fc3bcb64237eee54de614220aaf9208b23cbfb0
Manav Misra <manavm@visionwebsoft.com>
2020-08-27 13:52:39 -0500

    :sparkles: Add link to home

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from "react"
 
+import { Link } from "react-router-dom"
+
 export const Header = () => (
   <header>
     <section className="has-text-centered hero is-primary">
       <div className="hero-body">
         <div className="container">
-          <h1 className="has-text-info-dark is-family-secondary title">Todo List!</h1>
+          <h1 className="has-text-info-dark is-family-secondary title">
+            <Link to="/">Todo List!</Link>
+          </h1>
         </div>
       </div>
     </section>
