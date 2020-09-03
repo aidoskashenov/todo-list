7f97e51f242db519b230ebb0463d7c305cb7a5c0
Manav Misra <manavm@visionwebsoft.com>
2020-09-03 11:23:15 -0500

    :construction: Add logic with auth and TODOs for form submission

--- client/src/components/views/Login/index.js
+++ client/src/components/views/Login/index.js
@@ -27,9 +27,9 @@ export const Login = () => {
   }
 
   useEffect(() => {
-    auth.onAuthStateChanged(user => {
+    auth.onAuthStateChanged((user) => {
       if (user) {
-        history.push("/todos", {currentUser: user.email})
+        history.push("/todos", { currentUser: user.email })
       }
     })
   }, [history])
@@ -50,54 +50,98 @@ export const Login = () => {
           email: Yup.string()
             .email("Invalid email address!")
             .required("Email is required!"),
-          pass: !forgotMode && Yup.string().min(6).required("Pass is required!"),
+          pass:
+            !forgotMode && Yup.string().min(6).required("Pass is required!"),
         })}
-        onSubmit={(values, { setSubmitting }) => {
-          console.log("submission", values)
-          setSubmitting(false)
+        onSubmit={({ name, email, pass }, { setSubmitting }) => {
+          if (forgotMode) {
+            auth
+              .sendPasswordResetEmail(email)
+              .then((res) => {
+                // TODO: Create a notification to tell them to check their ✉️
+              })
+              .catch((err) => {
+                console.error(err)
+              })
+          } else if (loginMode) {
+            auth
+              .signInWithEmailAndPassword(email, pass)
+              .then(() => {
+                setSubmitting(false)
+                history.push("/todos", { currentUser: email })
+              })
+              .catch((err) => {
+                console.error(err)
+              })
+          } else {
+            auth
+              .createUserWithEmailAndPassword(email, pass)
+              .then(() => {
+                setSubmitting(false)
+                // TODO: Use our api to send 'name' and any other deets over to Mongo
+                // After this is sent to our Mongo...
+                // try-catch
+                // history.push("/todos", { currentUser: email })
+              })
+              .catch((err) => {
+                console.log(err)
+              })
+          }
         }}
       >
-        <Form>
-          {!loginMode ? (
+        {({ isSubmitting }) => (
+          <Form>
+            {!loginMode ? (
+              <div className="field">
+                <label htmlFor="name" className="ml-2">
+                  Name
+                </label>
+                <div className="control mx-2 my-1">
+                  <Field name="name" type="text" />
+                  <p className="help is-danger">
+                    <ErrorMessage name="name" />
+                  </p>
+                </div>
+              </div>
+            ) : null}
+
             <div className="field">
-              <label htmlFor="name" className="ml-2">
-                Name
+              <label htmlFor="email" className="ml-2">
+                Email
               </label>
               <div className="control mx-2 my-1">
-                <Field name="name" type="text" />
-                <p className="help is-danger">
-                  <ErrorMessage name="name" />
-                </p>
+                <Field name="email" type="email" />
+                <ErrorMessage name="email" />
               </div>
             </div>
-          ) : null}
 
-          <div className="field">
-            <label htmlFor="email" className="ml-2">
-              Email
-            </label>
-            <div className="control mx-2 my-1">
-              <Field name="email" type="email" />
-              <ErrorMessage name="email" />
-            </div>
-          </div>
+            {!forgotMode ? (
+              <div className="field">
+                <label htmlFor="pass" className="ml-2">
+                  Password
+                </label>
+                <div className="control mx-2 my-1">
+                  <Field name="pass" type="password" />
+                  <ErrorMessage name="pass" />
+                </div>
+              </div>
+            ) : null}
 
-          {!forgotMode ? <div className="field">
-            <label htmlFor="pass" className="ml-2">
-              Password
-            </label>
-            <div className="control mx-2 my-1">
-              <Field name="pass" type="password" />
-              <ErrorMessage name="pass" />
-            </div>
-          </div> : null}
-
-          <button type="submit" className="button is-success ml-2 mt-2">
-            Submit
-          </button>
-        </Form>
+            <button
+              type="submit"
+              className="button is-success ml-2 mt-2"
+              disabled={isSubmitting}
+            >
+              Submit
+            </button>
+          </Form>
+        )}
       </Formik>
-      <Options forgotMode={forgotMode}loginMode={loginMode} handler={handleToggle} />
+      <Options
+        forgotMode={forgotMode}
+        loginMode={loginMode}
+        handler={handleToggle}
+      />
     </section>
   )
 }
