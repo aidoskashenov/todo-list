b1b107b5ecbbd1ec8c38efd98c9ae43a4290091a
Manav Misra <manavm@visionwebsoft.com>
2020-09-04 12:17:28 -0500

    :fire: Remove code to check for duplicate ✉️s
    Letting 'auth' take care of this! 😃

--- server/db/index.js
+++ server/db/index.js
@@ -2,18 +2,7 @@ import client from './client';
 
 export const addUser = async (newUser) => {
   try {
-    // Pull out the user ✉️
-    const { email } = newUser;
-    const existingUser = await client
-      .db('todos')
-      .collection('users')
-      .findOne({ email });
-
-    if (!existingUser) {
-      return await client.db('todos').collection('users').insertOne(newUser);
-    }
-
-    throw new Error('User already exists!');
+    return await client.db('todos').collection('users').insertOne(newUser);
   } catch (err) {
     // Throw back any other errors
     throw new Error(err);
