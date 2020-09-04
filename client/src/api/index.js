35ed8d8020fa78516c15e94be58c4e76c2b58404
Manav Misra <manavm@visionwebsoft.com>
2020-09-04 08:37:34 -0500

    Adjust 'baseURL'
    Removing '/'

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,12 @@
 import dotenv from "dotenv"
 dotenv.config()
 
-const baseUrl = process.env.REACT_APP_BASE_URL
+const baseURL = process.env.REACT_APP_BASE_URL
 
 // Factory Function
 export default (route) => ({
   async create(payload) {
-    const res = await fetch(`${baseUrl}${route}/create` , {
+    const res = await fetch(`${baseURL}/${route}/create` , {
       method: 'POST',
       headers: {
       'Content-Type': 'application/json',
