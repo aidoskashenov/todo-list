0b42fe8b96b04a8f4f7d109a5036f5fbea6a1dca
Manav Misra <manavm@visionwebsoft.com>
2020-09-13 15:31:07 -0500

    :construction: Use Google Auth to try for super-admin

--- client/src/components/views/SuperAdmin.js
+++ client/src/components/views/SuperAdmin.js
@@ -1,11 +1,61 @@
-import React  from 'react'
+import React, { useEffect, useState } from "react"
 
-import {googleAuth} from "auth"
+import { useHistory } from "react-router"
 
-console.log('hi', googleAuth)
+import { auth, googleAuth } from "auth"
+
+import { Notification } from "components/base"
 
 export const SuperAdmin = () => {
+  const history = useHistory()
 
+  const [notification, setNotification] = useState(null)
+  const [secs, setSecs] = useState(3)
 
-  return <p>Hi!</p>
+  const updateNotification = (secs = 3) => {
+    setNotification({
+      className: "is-danger",
+      text: `Redirecting to login page in ${secs} seconds...`,
+    })
+  }
+
+  useEffect(() => {
+    if (!notification) {
+      auth
+        .signInWithPopup(googleAuth)
+        .then(({ user: { email } }) => {
+          if (email !== process.env.REACT_APP_SUPER_ADMIN_EMAIL) {
+            throw new Error()
+          }
+        })
+        .catch(() => {
+          updateNotification()
+        })
+    } else {
+      auth.signOut().then()
+    }
+  }, [notification])
+
+  useEffect(() => {
+    if (notification) {
+      while (secs >= 0) {
+        const intervalID = setInterval(() => {
+          setSecs((prevSecs) => prevSecs - 1)
+          updateNotification(secs)
+        }, 1000)
+
+        // Cleanup function
+        return () => {
+          clearInterval(intervalID)
+        }
+      }
+      history.push("/login")
+    }
+  }, [history, notification, secs])
+
+  return notification ? (
+    <div className="container">
+      <Notification notification={notification} />
+    </div>
+  ) : null
 }
