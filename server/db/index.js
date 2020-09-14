8da93ea72253a06da2f02f1413e0fffc9ebc6895
Manav Misra <manavm@visionwebsoft.com>
2020-09-14 08:13:52 -0500

    :fire: Delete all todos with user

--- server/db/index.js
+++ server/db/index.js
@@ -30,10 +30,17 @@ export const getUser = async (uid) => {
 
 export const deleteUser = async (uid) => {
   try {
-    return await client
+    const delUserRes = await client
       .db('todos')
       .collection('users')
       .deleteOne({ uid });
+
+    const delTodosRes = await client
+      .db('todos')
+      .collection('todos')
+      .deleteMany({ uid });
+
+    return { delUserRes, delTodosRes };
   } catch (err) {
     throw new Error(err);
   }
